3e5156ae8d73e1f107fc34f7c128ede15815702f
AdrianZ <aventhelast@gmail.com>
2019-09-19 15:44:27 +0200

    main file for styles added

---
+++ components/DateForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="datePicker__wrapper">
+    <p class="datePicker__label">
+      Dates
+    </p>
+    <div class="datePicker">
+      <p>
+        <span>Check In</span>
+        <span>Check Out</span>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DateForm',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.datePicker {
+  border: 1px solid #D6D6D6;
+
+  &__wrapper {
+    width: 300px;
+  }
+
+  &__label {
+    font-size: 12px;
+    font-weight: bold;
+  }
+}
+</style>
