219f81cb6196a43359b1aec25016d7e77a20981a
AdrianZ <aventhelast@gmail.com>
2019-09-19 23:03:24 +0200

    base form to active date-picker created

--- components/DateForm.vue
+++ components/DateForm.vue
@@ -3,12 +3,14 @@
     <p class="datePicker__label">
       Dates
     </p>
+    {{ openCalendar }}
     <div class="datePicker">
-      <p>
-        <span>Check In</span>
-        <span>Check Out</span>
+      <p class="datePicker__actions">
+        <span :class="['datePicker__btn', openCalendar === 1 ? 'datePicker__btn--active' : '']" @click="openCalendar = 1">Check In</span>
+        <span :class="['datePicker__btn', openCalendar === 2 ? 'datePicker__btn--active' : '']" @click="openCalendar = 2">Check Out</span>
       </p>
     </div>
+
   </div>
 </template>
 <script>
@@ -16,7 +18,7 @@ export default {
   name: 'DateForm',
   data () {
     return {
-
+      openCalendar: 0
     }
   }
 }
@@ -25,9 +27,28 @@ export default {
 <style lang="scss">
 .datePicker {
   border: 1px solid #D6D6D6;
+  height: 36px;
+  border-radius: 2px;
+  padding: 5px;
+  box-sizing: border-box;
+
+  &__actions {
+    display: flex;
+    justify-content: space-between;
+    line-height: 24px;
+  }
+
+  &__btn {
+    padding: 0 10px;
+    cursor: pointer;
+
+    &--active {
+      background: #A0DAD4;
+    }
+  }
 
   &__wrapper {
-    width: 300px;
+    width: 260px;
   }
 
   &__label {
